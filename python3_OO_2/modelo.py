bfb2c75afe8db80e5807aeb004fe6fa1dfcda8d7
Renato Ferreira de Assis <renato.assis@g.globo>
2021-04-08 20:05:05 -0300

    Change: news information

--- python3_OO_2/modelo.py
+++ python3_OO_2/modelo.py
@@ -52,18 +52,56 @@ class Serie(Programa):  # Classe Filho
         return f'{self.nome} - {self.ano} - {self.temporadas}temporadas - {self._likes}'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+# Herdando o comportamento do list (iteracão da lista)
+# Essa herança tras beneficios e problemas
+# class Playlist(list):
+#     def __init__(self, nome, programas):
+#         self.nome = nome
+#         super().__init__(programas)
+
+    # def tamanho(self):
+    #     return len(self.programas)
+
+
 vingadores = Filme('Vingadores-guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
 vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+vingadores.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
 
 # Polimorfismo (vingadores e atlanta são um programa)
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 
-for programa in filmes_e_series:
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
 
     # 1) Primeira vez
